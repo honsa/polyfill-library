ec4621bacb3129fccd09d768884bb89e5844d528
Jake Champion <me@jakechampion.name>
2019-12-20 21:51:02 +0000

    Make Object.entries and Object.getOwnPropertyDescriptor work with a StringObject

--- polyfills/Object/getOwnPropertyDescriptor/polyfill.js
+++ polyfills/Object/getOwnPropertyDescriptor/polyfill.js
@@ -22,7 +22,7 @@
 		// 1. Let obj be ? ToObject(O).
 		var obj = ToObject(O);
 		// Polyfill.io fallback for non-array-like strings which exist in some ES3 user-agents (IE 8)
-		var obj = Type(obj) === 'string' && toString.call(O) == '[object String]' ? split.call(O, '') : Object(O);
+		var obj = (Type(obj) === 'string' || obj instanceof String) && toString.call(O) == '[object String]' ? split.call(O, '') : Object(O);
 
 		// 2. Let key be ? ToPropertyKey(P).
 		var key = ToPropertyKey(P);
